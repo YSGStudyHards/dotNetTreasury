6b49fce9c1819b97f5f9e6a021fe69132ea819c6
your name <your_email@youremail.com>
2019-03-27 10:23:41 +0800

    delegate

---
+++ C#，.Net，.Net Core 编程练习/深入了解委托和事件/委托的调用/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace 委托的调用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[5] {1,3,6,9,10 };
+            Console.WriteLine(GetMax(t));
+        }
+
+
+        /// <summary>
+        /// 求最大值
+        /// </summary>
+        /// <param name="Nums"></param>
+        /// <returns></returns>
+        static int GetMax(int[] Nums)
+        {
+            int max = Nums[0];
+            for (int i = 0; i < Nums.Length; i++)
+            {
+                if (Nums[i]>max)
+                {
+                    max = Nums[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ C#，.Net，.Net Core 编程练习/深入了解委托和事件/平台内置委托action和func/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 平台内置委托action和func
+{
+    //定义委托
+    public delegate void WriteLine();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //声明委托
+            //WriteLine writeLine = new WriteLine(tt);
+            //writeLine();//调用委托
+            //Console.WriteLine();
+            WriteLine t = tt;
+            t();
+        }
+
+        static void tt()
+        {
+            Console.WriteLine("一个人");
+        }
+    }
+}
